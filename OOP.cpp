c9101a4e003cd0c706af430587ed97e849bc16b0
phamtrunghoa <90266279+phamtrunghoa@users.noreply.github.com>
2022-08-22 16:12:03 +0700

    Update OOP.cpp

--- OOP.cpp
+++ OOP.cpp
@@ -134,3 +134,60 @@ int main()
     cout << tong << "\n";
     return 0;
 }
+
+
+//***************************************//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Number
+{
+private:
+    int num1;
+    int num2;
+
+public:
+    int Getnum1()
+    {
+        return num1;
+    }
+    void Setnum1(int Num1)
+    {
+        num1 = Num1;
+    }
+    int Getnum2()
+    {
+        return num2;
+    }
+    void Setnum2(int Num2)
+    {
+        num2 = Num2;
+    }
+    Number Add(const Number &a)
+    {
+        Number result;
+        result.num1 = num1 + a.num2;
+        return result;
+    }
+};
+
+int main()
+{
+    // int tong;
+    // Number num, num2;
+    // num.Setnum1(10);
+    // num2.Setnum1(20);
+    // tong = num.Add(num2);
+    // cout << tong << "\n";
+    // return 0;
+    Number tong;
+    Number num, num2;
+    num.Setnum1(10);
+    num2.Setnum2(40);
+    tong = num.Add(num2);
+    cout << tong.Getnum1() << "\n";
+    return 0;
+}
